c511377ce5b63666fb37e73985afb68ab67d7a9e
Paulvz22 <601731@student.belgiumcampus.ac.za>
2026-07-28 16:33:28 +0200

    Initial commit with clean MAUI MVC and Unity structure

---
+++ MAUIProject/BCOpendayApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BCOpendayApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MAUIProject/BCOpendayApp/Views/temp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//This file is just a temporary file to ensure that Github detect the created folder 
+namespace BCOpendayApp.Views
+{
+    internal class temp
+    {
+    }
+}
