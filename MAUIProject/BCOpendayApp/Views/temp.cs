29c75d00758cae6bc0ab886d29f753311180ba64
Paulvz22 <601731@student.belgiumcampus.ac.za>
2026-08-01 21:49:32 +0200

    Fixed gitignore deleting .sln and .csproj files
    Re-added the deleted files and fixed the gitignore

--- MAUIProject/BCOpendayApp/Views/temp.cs
+++ MAUIProject/BCOpendayApp/Views/temp.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 //This file is just a temporary file to ensure that Github detect the created folder 
 namespace BCOpendayApp.Views
 {
